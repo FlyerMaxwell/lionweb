df97a5726acab7f0a0c4c7583987f09fa7d0decf
MirrorrorriN <somunsfool@163.com>
2017-09-21 13:36:10 +0800

    modification

---
+++ src/main/src/main/java/com/lion/HomeController.java
@@ -0,0 +1,11 @@
+package com.lion;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    public void home(){
+    }
+}
